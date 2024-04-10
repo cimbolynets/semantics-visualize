2501af3647affbe4edf0f7a82abb83d008c6d289
Vitalii Tsimbolynets <cimbolynets@gmail.com>
2024-04-10 18:42:55 +0200

    fixed tabs for NS scopes

--- src/components/output/Scopes.tsx
+++ src/components/output/Scopes.tsx
@@ -1,8 +1,7 @@
 import { BetweenHorizontalStart } from "lucide-react";
-import { useState } from "react";
 import { Button } from "../ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
-import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { EnvsInfo } from "./EnvsInfo";
 import { StatesInfo } from "./StatesInfo";
 
@@ -14,8 +13,6 @@ interface ScopesProps {
 }
 
 export default function Scopes({ states, envs }: ScopesProps) {
-  const [tab, setTab] = useState<Tabs>("states");
-
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -24,22 +21,18 @@ export default function Scopes({ states, envs }: ScopesProps) {
         </Button>
       </SheetTrigger>
       <SheetContent side="right" className="transition-all">
-        <ToggleGroup
-          type="single"
-          value={tab}
-          onValueChange={(v: Tabs) => setTab(v)}
-          className="my-4 grid grid-cols-2 gap-2 w-full"
-        >
-          <ToggleGroupItem value="states">States</ToggleGroupItem>
-          <ToggleGroupItem value="envs">Environments</ToggleGroupItem>
-        </ToggleGroup>
-        {tab === "states" ? (
-          <StatesInfo states={states} />
-        ) : tab === "envs" ? (
-          <EnvsInfo envs={envs} />
-        ) : (
-          "No such tab"
-        )}
+        <Tabs defaultValue="states">
+          <TabsList className="w-full my-4">
+            <TabsTrigger value="states" className="basis-0 grow">States</TabsTrigger>
+            <TabsTrigger value="envs" className="basis-0 grow">Environments</TabsTrigger>
+          </TabsList>
+          <TabsContent value="states" asChild>
+            <StatesInfo states={states} />
+          </TabsContent>
+          <TabsContent value="envs" asChild>
+            <EnvsInfo envs={envs} />
+          </TabsContent>
+        </Tabs>
       </SheetContent>
     </Sheet>
   );
