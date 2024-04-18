5ddb326835027e1e37f568762934091177ee1202
Vitalii Tsimbolynets <cimbolynets@gmail.com>
2024-04-18 15:24:09 +0200

    fixed being unable to execute program after error

--- src/components/output/Output.tsx
+++ src/components/output/Output.tsx
@@ -1,14 +1,28 @@
 import { InterpreterError } from "@/interpreter/InterpreterError";
 import { useProgramStorage } from "@/lib/storage/programStorage";
 import { cn } from "@/lib/utils";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, useEffect, useRef } from "react";
 import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 import { EditorError } from "../EditorError";
 import { RenderAM } from "./modes/RenderAM";
 import { RenderNS } from "./modes/RenderNS";
 import { RenderSOS } from "./modes/RenderSOS";
 
-function ErrorComponent({ error }: FallbackProps) {
+function ErrorComponent({ error, resetErrorBoundary }: FallbackProps) {
+  const prevProgramId = useRef<number | undefined>();
+  const programId = useProgramStorage((state) => state.programId);
+
+  useEffect(() => {
+    if (
+      typeof prevProgramId.current !== "undefined" &&
+      error &&
+      programId !== prevProgramId.current
+    ) {
+      resetErrorBoundary();
+    }
+    prevProgramId.current = programId;
+  }, [programId]);
+
   return (
     <>
       {error instanceof InterpreterError ? (
