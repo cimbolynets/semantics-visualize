c98b96c2dc6317f6544d8f72acf1f99881dd0794
Vitalii Tsimbolynets <cimbolynets@gmail.com>
2024-05-22 19:47:40 +0200

    fixed help tab items layout on mobile

--- src/components/Help/HelpItem.tsx
+++ src/components/Help/HelpItem.tsx
@@ -8,10 +8,10 @@ interface HelpItemProps {
 
 export const HelpItem: FC<HelpItemProps> = ({ children, description }) => {
   return (
-    <div className="grid grid-cols-12 items-center gap-4">
-      <div className="col-span-4">{children}</div>
-      <Separator className="max-w-[2rem] mx-auto col-span-2" />
-      <span className="col-span-6">{description}</span>
+    <div className="flex flex-col lg:grid grid-cols-12 lg:items-center gap-4">
+      <div className="lg:col-span-4">{children}</div>
+      <Separator className="max-w-[2rem] mx-auto col-span-2 hidden lg:block" />
+      <span className="lg:col-span-6">{description}</span>
     </div>
   );
 };
